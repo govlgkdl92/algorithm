fa944f2dcda303063411355ce9e59ad6ee5fea01
KimYiseul <74230238+govlgkdl92@users.noreply.github.com>
2022-04-19 09:37:32 +0900

    0419

--- src/org/success/Sosu.java
+++ src/org/success/Sosu.java
@@ -21,15 +21,16 @@ public class Sosu {
         int number = n;
         int arr[] = new int[number+1];
 
-        // 1. 배열을 생성하여 초기화한다.
+        // 배열 생성
         for(int i=2;i<=number;i++) {
             arr[i] = i;
         }
 
-        // 2. 2부터 시작해서 특정 수의 배수에 해당하는 수를 모두 지운다.
-        // (지울 때 자기자신은 지우지 않고, 이미 지워진 수는 건너뛴다.)
+        // 1은 소수가 아니므로 2부터 시작, 특정 수의 배수에 해당하는 수를 모두 지우기
         for(int i=2;i<=number;i++) {
-            if(arr[i]==0) continue; // 이미 지워진 수라면 건너뛰기
+            if(arr[i]==0) {
+                continue; // 이미 지워진 수라면 건너뛰기
+            }
 
             // 이미 지워진 숫자가 아니라면, 그 배수부터 출발하여, 가능한 모든 숫자 지우기
             for(int j=2*i;j<=number; j+=i) {
@@ -37,17 +38,14 @@ public class Sosu {
             }
         }
 
-        // 3. 2부터 시작하여 남아있는 수를 모두 출력한다.
+        // 2부터 시작하여 남아있는 수를 모두 출력
         for(int i=2;i<=number;i++) {
             if(arr[i]!=0) {
                 answer++;
             }
         }
-
         return answer;
     }
-
-
 
     //main
     public static void main(String[] args) {
