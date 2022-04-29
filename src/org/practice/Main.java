d4f282add5e0070b06136cd42ae47a265b7cb2f4
govlgkdl92 <govlgkdl92@gmail.com>
2022-04-29 16:18:25 +0900

    0429

--- src/org/practice/Main.java
+++ src/org/practice/Main.java
@@ -1,8 +1,21 @@
 package org.practice;
 
+import org.success.Unfinished;
+
 public class Main {
 
     public static void main(String[] args) {
+        Unfinished unfinished = new Unfinished();
+
+        String[] n = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] m = {"josipa", "filipa", "marina", "nikola"};
+
+        String solution = unfinished.solution(n, m);
+
+        System.out.println(solution);
+    }
+
+/*    public static void main(String[] args) {
         ArrayString arrayString = new ArrayString();
 
         String[] n = {"sun", "bed", "car"};
@@ -14,5 +27,5 @@ public class Main {
         for (int i = 0; i < solution.length; i++) {
             System.out.print(solution[i] + " ");
         }
-    }
+    }*/
 }
