119238c99d149516dd5d4b2ac7f5a7b8d2c5c73e
govlgkdl92 <govlgkdl92@gmail.com>
2022-06-08 09:59:49 +0900

    0607

--- src/org/practice/Main.java
+++ src/org/practice/Main.java
@@ -6,17 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         KArray quiz = new KArray();
-        DivisionOne divisionOne = new DivisionOne();
+        Minority quiz2 = new Minority();
 
         int[] arr = {1, 5, 2, 6, 3, 7, 4};
         int[][] arr2 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        int[] arr3 = {1,2,7,6,4};
 
         int[] solution = quiz.solution(arr, arr2);
+        int solution2 = quiz2.solution(arr3);
 
         System.out.println();
         for (int i = 0; i < solution.length; i++) {
             System.out.print("KArray["+i+"]: "+solution[i] + " ");
         }
+
+        System.out.println();
+        System.out.println("Minority: "+solution2);
     }
 
 }
